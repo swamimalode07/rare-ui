0da38a1e0abb93f965043e726dbcd2de5e027305
swamimalode07 <swamimalode2004@gmail.com>
2026-07-22 01:04:11 +0530

    aligned duration picker component to center in mobile

--- app/components/durationpicker/demo.tsx
+++ app/components/durationpicker/demo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const DurationPickerPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen rounded-2xl">
+    <div className="flex h-full flex-col items-center justify-center rounded-2xl">
       <DurationPicker />
     </div>
   )
